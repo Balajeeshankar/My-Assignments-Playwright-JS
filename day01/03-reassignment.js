266eb70620471291f8c32b64e09627328e742b45
Balajeeshankar <balajeeshankar96@gmail.com>
2024-12-18 14:21:27 +0530

    Day1

---
+++ day01/03-reassignment.js
@@ -0,0 +1,33 @@
+var company = "MacInd";
+var company = "MacPkn";
+
+/**
+ * var allows redeclaration of the Variables
+ * var allows reasssignment of the Variables
+ */
+
+console.log(company);
+
+/**------------------------------------------------------------------- */
+
+let ProfName = "QA" ;
+ProfName = "Dev";
+
+console.log(ProfName);
+
+/**
+ * let will allow the reassignment of the variables
+ * let will not allow the redeclaration of the variables.
+ */
+
+const Password = 'Abc@123';
+Password = 'Efgh@123';
+console.log(Password);
+
+/**
+ * Const will not allow the reassignment of the variables
+ * Const will not allow the redeclaration of the variables.
+ */
+
+
+
