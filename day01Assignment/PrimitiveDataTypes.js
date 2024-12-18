0c69e955eae37a7815871ad5479a6fd29ec9bddc
Balajeeshankar <balajeeshankar96@gmail.com>
2024-12-18 16:02:43 +0530

    Add files via upload

---
+++ day01Assignment/PrimitiveDataTypes.js
@@ -0,0 +1,13 @@
+let firstName = "Balaji";
+let CompanyName = "MacInd";
+let MobileNumber = 9047478201;
+let IsAutomation = true;
+let hasPlaywright;
+
+
+console.log(typeof firstName + " "+ "is the Datatype of firstname");
+console.log(typeof CompanyName + " "+ "is the Datatype of CompanyName");
+console.log(typeof MobileNumber + " "+ "is the Datatype of MobileNumber");
+console.log(typeof IsAutomation + " "+ "is the Datatype of IsAutomation");
+console.log(typeof hasPlaywright + " "+ "is the Datatype of hasPlaywright");
+
